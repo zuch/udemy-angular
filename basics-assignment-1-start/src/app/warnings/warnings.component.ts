52a9add4cc50f23bb3a64021b9617d27e5c9ad9f
Sascha Watermeyer <external.sascha.watermeyer@kuehne-nagel.com>
2023-12-18 16:56:10 +0100

    28

---
+++ basics-assignment-1-start/src/app/warnings/warnings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warnings',
+  templateUrl: './warnings.component.html',
+  styleUrl: './warnings.component.css'
+})
+export class WarningsComponent {
+
+}
---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+}
